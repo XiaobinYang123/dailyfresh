d3c1564230eb0b37b9ee9f77317bfcc6a9f051d9
q632346824 <632346824@qq.com>
2019-09-05 15:07:09 +0800

    f1

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -24,9 +24,9 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 SECRET_KEY = 'o)g+l0_^kcml+#g9)kr$mppef2ptxkrqk90p!&5%^%kq4i37+1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
